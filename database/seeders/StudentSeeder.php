cb2b83955580f25cfbb4987c864b08a3e40e1bf7
thanakorn_pr <thanakorn.prathumchai@dentsu.com>
2022-09-10 00:55:44 +0700

    add migration and seed

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->insert([
+            ['name' => "John doe"],
+            ['name' => "Pual Nano"],
+            ['name' => "David Rean"]
+        ]);
+    }
+}
